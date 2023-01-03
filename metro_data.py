3bbd2efcb2677db980036f8782d69a7957b1d6a4
CarmeloChou <41857762+CarmeloChou@users.noreply.github.com>
2023-01-03 16:07:48 +0800

    Update metro_data.py

--- metro_data.py
+++ metro_data.py
@@ -13,13 +13,10 @@ def get_data(url):
 
 if __name__ == '__main__':
     print(time.time())
-    while True:
-        if int(time.time())%3600 == 0:
-            results = get_data('http://dtkl.shmetro.com:9091/data/getLineTop5Data')
-            start = time.strftime('%Y-%m-%d-%H-%M-%S')
-            print(start)
-            with open('./%s.txt'%start, 'w', encoding='utf-8') as w:
-                json.dump(results, w, ensure_ascii=False, indent=4)
-            time.sleep(3500)
-        else:
-            pass
+    results = get_data('http://dtkl.shmetro.com:9091/data/getLineTop5Data')
+    start = time.strftime('%Y-%m-%d-%H-%M-%S')
+    print(start)
+    with open('%s.txt'%start, 'w', encoding='utf-8') as w:
+        json.dump(results, w, ensure_ascii=False, indent=4)
+
+
