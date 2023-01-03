28eb8d23a01b921484c8359073160fb6cee46cec
CarmeloChou <41857762+CarmeloChou@users.noreply.github.com>
2023-01-03 21:04:32 +0800

    Update metro_data.py

--- metro_data.py
+++ metro_data.py
@@ -6,7 +6,7 @@ def get_data(url):
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36'
     }
-    response = requests.get(url)
+    response = requests.get(url, headers=headers)
     # print(response.text)
     return response.text
 
